83ab2e57fc9dd4731b816a21d7859004c603abae
ABouzit <46455125+ABouzit@users.noreply.github.com>
2020-03-04 01:54:33 +0100

    asjdb

--- src/views/ConnexionPageDesktop/Sections/Publier.js
+++ src/views/ConnexionPageDesktop/Sections/Publier.js
@@ -178,10 +178,10 @@ class Publier extends React.Component {
     ).then(res => {
       let s = res.data.filePath.replace("\\", "/").replace("\\", "/");
       return Axios.post(config.API_URL + "histoires", {
-        // userText: { id: "3748b6ad-9fcd-4bf5-878a-3ea9aa00952d" },
-        //userDessin: { id: "3748b6ad-9fcd-4bf5-878a-3ea9aa00952d" }, //id user serveur
-        userText: { id: "4305f81f-8e67-45df-80eb-54a646387457" },
-        userDessin: { id: "4305f81f-8e67-45df-80eb-54a646387457" },
+        userText: { id: "3748b6ad-9fcd-4bf5-878a-3ea9aa00952d" },
+        userDessin: { id: "3748b6ad-9fcd-4bf5-878a-3ea9aa00952d" }, //id user serveur
+        //userText: { id: "4305f81f-8e67-45df-80eb-54a646387457" },
+        //userDessin: { id: "4305f81f-8e67-45df-80eb-54a646387457" },
         //userText: { id: "3c500b25-cb58-4be3-861e-2bb2926bd75f" },//serv2
         //userDessin: { id: "3c500b25-cb58-4be3-861e-2bb2926bd75f" },
         lienIllustration: config.API_URL + s,
